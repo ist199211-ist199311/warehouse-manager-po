0addce0a3976efa25cecdc914e582e08e6e94801
Diogo Correia <me@diogotc.com>
2021-10-24 19:07:23 +0100

    Show files that haven't been added to CVS

--- update_cvs.sh
+++ update_cvs.sh
@@ -2,6 +2,8 @@
 
 # THIS SCRIPT ONLY WORKS WITH ZSH
 
+export CVS_RSH=ssh
+
 CVS_FOLDER=../project
 
 removed_files() {
@@ -12,8 +14,33 @@ removed_files() {
 		| grep -E '\.java$' \
 }
 
+# wrapper to update cvs quietly from the (git) repo root
+cvs_update() {
+	# run in subshell to keep cwd intact
+	(cd $CVS_FOLDER && cvs -q update)
+}
+
 cp --parents **/*.java $CVS_FOLDER/
 
-echo "The following files have been deleted from Git but are still in CVS:"
-removed_files
+echo Changes to commit:
+cvs_update
+echo
 
+# detect files that are not in git but are in CVS
+if removed_files &>/dev/null; then
+	echo "The following files have been deleted from Git but are still in CVS:"
+	echo
+	removed_files | xargs -n1 echo rm
+  echo
+fi
+
+# detect files not added to CVS
+if cvs_update |& grep -w '?' &>/dev/null; then
+	echo "You have new files to add to CVS first:"
+	echo
+	cvs_update |& grep -w '?' | cut -d' ' -f2 | xargs -n1 echo cvs add
+  echo
+fi
+
+echo Then, commit with
+echo 'cvs commit -m "'"$(git log -1 --pretty="%B - From Git by %an at %ai")"'"'
