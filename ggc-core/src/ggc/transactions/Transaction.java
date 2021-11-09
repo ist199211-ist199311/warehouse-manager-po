570a118a5eac3ee5742829cdc08614ef9ac4ee20
Rafael Oliveira <rafdev.x@gmail.com>
2021-11-09 20:56:25 +0000

    make transactions serializable

--- ggc-core/src/ggc/transactions/Transaction.java
+++ ggc-core/src/ggc/transactions/Transaction.java
@@ -5,9 +5,16 @@ import ggc.products.Batch;
 import ggc.products.Product;
 import ggc.util.Visitable;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Optional;
 
-public abstract class Transaction implements Visitable {
+public abstract class Transaction implements Serializable, Visitable {
+  /**
+   * Serial number for serialization.
+   */
+  @Serial
+  private static final long serialVersionUID = 202111092049L;
 
   private final int id;
   private final double value;
