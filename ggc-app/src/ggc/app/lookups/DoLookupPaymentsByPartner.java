c47d4cc6f78fe06055adf3ca643d6e9886dde79f
Diogo Correia <me@diogotc.com>
2021-11-12 00:35:39 +0000

    Stringify transaction list on lookup payments by partner

--- ggc-app/src/ggc/app/lookups/DoLookupPaymentsByPartner.java
+++ ggc-app/src/ggc/app/lookups/DoLookupPaymentsByPartner.java
@@ -1,14 +1,17 @@
 package ggc.app.lookups;
 
+import ggc.WarehouseManager;
+import ggc.app.Stringifier;
+import ggc.app.exceptions.UnknownPartnerKeyException;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-import ggc.WarehouseManager;
-import ggc.app.exceptions.UnknownPartnerKeyException;
 
 /**
  * Lookup payments by given partner.
  */
 public class DoLookupPaymentsByPartner extends Command<WarehouseManager> {
+
+  private final Stringifier stringifier = new Stringifier();
 
   public DoLookupPaymentsByPartner(WarehouseManager receiver) {
     super(Label.PAID_BY_PARTNER, receiver);
@@ -18,7 +21,10 @@ public class DoLookupPaymentsByPartner extends Command<WarehouseManager> {
   @Override
   public void execute() throws CommandException {
     try {
-      this._receiver.lookupPaymentsByPartner(this.stringField("partnerId"));
+      this._receiver.lookupPaymentsByPartner(this.stringField("partnerId"))
+              .stream()
+              .map(v -> v.accept(stringifier))
+              .forEach(_display::popup);
     } catch (ggc.exceptions.UnknownPartnerKeyException e) {
       throw new UnknownPartnerKeyException(e.getKey());
     }
